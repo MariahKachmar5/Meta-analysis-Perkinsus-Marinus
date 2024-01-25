134a4ea070948974b75e016f76db49ab35b6193a
Mariah Kachmar <mariahk1@umbc.edu>
2024-01-25 11:12:50 -0500

    fixes

--- Modeling_Perkinsus.R
+++ Modeling_Perkinsus.R
@@ -5,7 +5,7 @@
 #Description: Generalized Linear Mixed Effects Model Perkinsus marinus Meta-analysis 
 
 #Set working directory
-setwd("~/Documents/UMBC/GitHub/Meta-analysis-Perkinsus-Marinus/scripts")
+setwd("~/Documents/UMBC/GitHub/Meta-analysis-Perkinsus-Marinus/")
 
 #Load all packaged
 library(lme4)
